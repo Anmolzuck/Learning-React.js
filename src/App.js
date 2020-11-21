e1cca23192387647acb573e7ef85d5497ea2ba7c
Anmol Zakie <anmolzuck17@gmail.com>
2020-11-21 14:15:30 +0530

    added states

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import "./App.css";
 import Person from "./Person/person";
 
 class App extends Component {
+  /*state would be a special property,thus far we don't use it in a special way though, we can change this. State can be changed and if it changes
+and that's the special thing about it and it only works on that state property, if it changes, it will lead React to re-render our DOM or to update the DOM I should say.
+*/
+  state = {
+    persons: [{ name: "Anmol" }, { name: "Aarav" }],
+  };
+
+  switchNameHandler = () => {
+    console.log("Was clicked");
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi i am react app</h1>
         <p>This is really working</p>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} />
+        <Person name={this.state.persons[1].name}>My hobby is painting</Person>
       </div>
     );
 
--- src/Person/person.js
+++ src/Person/person.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 //Creating a component
-const person = () => {
-  return <p>I' am person!</p>;
+const person = (props) => {
+  return (
+    <div>
+      <p>
+        I am {props.name} and i am {Math.floor(Math.random() * 30)} years old!{" "}
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 export default person;

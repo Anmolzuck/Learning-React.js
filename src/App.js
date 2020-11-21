658279e0ccb758f645655e8f0e15a1952652e44c
Anmol Zakie <anmolzuck17@gmail.com>
2020-11-21 12:25:36 +0530

    Changed app.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi i am react app</h1>
+        <p>This is really working</p>
       </div>
     );
+
+    //internally html is converted into js by compiler(below is js code). But we dont use this bcuz it is comlex
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, `I' am react app!`)
+    // );
   }
 }
 

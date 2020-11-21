48c89c5316a599a5c4714064b39ef415aa171506
Anmol Zakie <anmolzuck17@gmail.com>
2020-11-21 14:18:57 +0530

    Added states

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   /*state would be a special property,thus far we don't use it in a special way though, we can change this. State can be changed and if it changes
 and that's the special thing about it and it only works on that state property, if it changes, it will lead React to re-render our DOM or to update the DOM I should say.
 */
+
   state = {
     persons: [{ name: "Anmol" }, { name: "Aarav" }],
   };
@@ -24,7 +25,7 @@ and that's the special thing about it and it only works on that state property, 
       </div>
     );
 
-    //internally html is converted into js by compiler(below is js code). But we dont use this bcuz it is comlex
+    //internally html is converted into js by compiler(below is js code). But we dont use this bcuz it is complex
     // return React.createElement(
     //   "div",
     //   { className: "App" },

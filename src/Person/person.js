eb1707b32ae68279373f44c7b1e24af6cde528f5
Anmol Zakie <anmolzuck17@gmail.com>
2020-12-04 13:58:19 +0530

    Old App.js file till vid -67

--- src/Person/person.js
+++ src/Person/person.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import Radium from 'radium';
-import './Person.css';
+import styled from 'styled-components';
+// import Radium from 'radium';
+// import './Person.css';
 
-//Creating a component
+//StyledDiv is a react component
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
+
 const person = (props) => {
-  const styles = {
-    '@media(min-width:500px)': {
-      width: '450px',
-    },
-  };
   return (
-    <div className="Person" style={styles}>
+    // <div className="Person" style={styles}>
+    <StyledDiv>
       <p onClick={props.click}>
         I am {props.name} and i am {Math.floor(Math.random() * 30)} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.onChange} value={props.name} />
-    </div>
+    </StyledDiv>
   );
 };
 
-export default Radium(person);
+export default person;

6d757243006c677c325533cb7f1cca3e5c325814
Anmol Zakie <anmolzuck17@gmail.com>
2020-12-05 12:03:46 +0530

    Passing condtions to styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component /*useState*/ } from 'react';
 import './App.css';
-// import Radium, { StyleRoot } from 'radium';
+import styled from 'styled-components';
 import Person from './Person/Person';
+
+//We can pass dynamic content in the styled component normally using ${}
+const StyledButton = styled.button`
+background-color: ${(props) => (props.alt ? 'red' : 'green')};
+color:white;
+font: inherit;
+border:1px solid blue;
+padding: 8px;
+cursor: pointer;
+
+  &:hover {
+    background-color:  ${(props) => (props.alt ? 'salmon' : 'lightgreen')};
+    color:black;
+  },
+`;
 
 class App extends Component {
   state = {
@@ -15,16 +30,12 @@ class App extends Component {
   };
 
   nameChangedHandler = (event, id) => {
-    //Find that person from the persons array
     const personIndex = this.state.persons.findIndex((el) => {
       return el.id === id;
     });
     console.log(personIndex);
 
     const person = { ...this.state.persons[personIndex] };
-
-    //Or we can aslo use - const person = Object.assign({},this.state.persons[personIndex])
-
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
@@ -41,36 +52,18 @@ class App extends Component {
   };
 
   deletePersonHandler = (personIndex) => {
-    //Always create a copy of the original array and then mutate the copied array by using slice() or spread operator
-    //const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
-    persons.splice(personIndex, 1); //create new version of the aaray
+    persons.splice(personIndex, 1);
     this.setState({ persons: persons });
   };
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      //   ':hover': {
-      //     backgroundColor: 'lightgreen',
-      //     color: 'black',
-      //   },
-    };
-
-    //Instead of rendering content conditionally by ternary operator we can use js
     let persons = null;
 
-    //Rendering the list dynamically
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            //This maps the persons array and returns each and every value and then renders it
             return (
               <Person
                 click={() => this.deletePersonHandler(index)}
@@ -82,30 +75,22 @@ class App extends Component {
           })}
         </div>
       );
-
-      //Applying dynamic styling
-      style.backgroundColor = 'red';
-      //   style[':hover'] = {
-      //     backgroundColor: 'salmon',
-      //     color: 'black',
-      //   };
     }
-
-    //Changing class dynamically
-    // let classes = ["red", "bold"].join(" ");  This will give "red bold" which is used in css
 
     const classes = [];
     if (this.state.persons.length <= 2) classes.push('red');
-
     if (this.state.persons.length <= 1) classes.push('bold');
 
     return (
       <div className="App">
         <h1>Hi i am react app</h1>
         <p className={classes.join(' ')}>This is really working</p>
-        <button style={style} onClick={this.tooglePersonHandler}>
+        <StyledButton
+          alt={this.state.showPersons}
+          onClick={this.tooglePersonHandler}
+        >
           Toogle name
-        </button>
+        </StyledButton>
         {persons}
       </div>
     );
